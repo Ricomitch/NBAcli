84ac3f8d71af238c4ee738d228c3e6a4ddfa9b1d
Rico Mitchell <ricomitch@icloud.com>
2020-03-03 04:30:36 +0000

    add notes for cli class

--- lib/NBAcli/cli.rb
+++ lib/NBAcli/cli.rb
@@ -1,8 +1,28 @@
 class NBAcli::CLI
   
+  #start should greet user, or give desc
+  # get data from api
+  # create new custom objects
+  #all inside start method
+  
   def start 
     puts "Hello"
-    binding.pry
+    puts "GETTING DATA FROM API ... PLEASE WAIT"
+    puts "CREATING NEW OBJS"
   end
+  
+  
+  # deal with inputs(loop to get new info)
+  #example while input =/= "exit do"
+  # display a list of somthing or give example of what we expect as inputs
+  # get user input
+  # depending on what we get, do something
+  # condition to check input for good value
+  # else tell then try 
+  
+  # exit command
+  # if input is == "exit"
+  # kill program say good bye
+  
   
 end  
