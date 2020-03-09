efd5c92d68532b5fc9fb995b090ff2609a0b4d53
Rico Mitchell <ricomitch@icloud.com>
2020-03-09 03:05:27 +0000

    made API call

--- lib/NBAcli/api.rb
+++ lib/NBAcli/api.rb
@@ -5,6 +5,10 @@
 # send those back to our cli 
 
 class NBAcli::API
-  def self.get_players
-    @players_hash = httparty
+  def self.get_games
+    @games_hash = Unirest.get "https://api-nba-v1.p.rapidapi.com/games/date/%7Bdate%7D",
+  headers:{
+    "X-RapidAPI-Host" => "api-nba-v1.p.rapidapi.com",
+    "X-RapidAPI-Key" => "e20bd75f02msh1193f74400a595bp13abe8jsnd35b87c78c4e"
+  }
 end  
